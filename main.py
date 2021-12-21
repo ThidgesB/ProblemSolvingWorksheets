31b40e82a39714d0eb1fe990ec9069386a828040
ckatjrdavis <ckatjrdavis@gmail.com>
2021-12-21 11:43:07 -0600

    added function that reverses string in argument

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
+#I'm still not sure exactly what a "Slice" is, but this formula iterates over letters designated by the integer(index position) within the brackets.
+#since the integer is set to -1, it starts at the last letter in the string, and goes backwards one by one, and returns the string that way.
+#if we were to change that number within the brackets to -2, with would skip every other letter in teh string, and return the letters it did not skip.
+#I.E. [::-2] would print "suSka", however it always starts at the -1 index position. 
+#a positive number would have a similar effect, however it always starts at Index position 0
+
+def reverse_a_string(reversed_string):
+    return reversed_string[::2]
+
+favorite_game = 'Dark Souls'
+
+reverse_a_string(favorite_game)
+print(reverse_a_string(favorite_game))
